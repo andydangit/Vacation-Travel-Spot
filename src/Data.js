4ae790386cca02bda1d26a925e66f0070dd2f913
pandoragear <andy.dang.it@gmail.com>
2022-03-07 23:22:08 -0600

    Feat: import data files

---
+++ src/Data.js
@@ -0,0 +1,65 @@
+"export default" [
+  {
+    Id: 1,
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+
+  {
+    Id: 2,
+    title: "Ha Long Bay",
+    location: "Vietnam",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/H%E1%BA%A1+Long+Bay/@20.8432386,106.8701231,10z/data=!3m1!4b1!4m5!3m4!1s0x314a5796518cee87:0x55c6b0bcc85478db!8m2!3d20.9100512!4d107.1839024 ",
+    startDate: "",
+    endDate: "",
+    description:
+      " Ha Long Bay  is known for its emerald waters and thousands of towering limestone islands topped by rainforests. ",
+    imageUrl: "https://unsplash.com/photos/mgHW9AIBA18",
+  },
+
+  {
+    Id: 3,
+    title: "Maldives GateWay",
+    location: "Maldives",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Maldives/@3.1025792,68.7534806,6z/data=!3m1!4b1!4m5!3m4!1s0x24b599bfaafb7bbd:0x414509e181956289!8m2!3d3.202778!4d73.22068",
+    startDate: "",
+    endDate: "",
+    description:
+      "Maldives, is an archipelagic country in the Indian subcontinent of Asia, situated in the Indian Ocean. It lies southwest of Sri Lanka and India, about 750 kilometres from the Asian continent's mainland",
+    imageUrl: "https://unsplash.com/photos/k9Zeq6EH_bk",
+  },
+
+  {
+    Id: 4,
+    title: "Hawaii Island",
+    location: "Hawaii",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Hawaii/@20.4025299,-161.9895576,6z/data=!3m1!4b1!4m5!3m4!1s0x7bffdb064f79e005:0x4b7782d274cc8628!8m2!3d19.8967662!4d-155.5827818",
+    startDate: "",
+    endDate: "",
+    description:
+      "Hawaii is a state in the Western United States, located in the Pacific Ocean about 2,000 miles from the U.S. mainland. ",
+    imageUrl: "https://unsplash.com/photos/kEYJs-yYUBU ",
+  },
+
+  {
+    Id: 5,
+    title: "Bora Bora",
+    location: "French Island",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Bora+Bora/@-16.5044211,-151.7735829,13z/data=!3m1!4b1!4m5!3m4!1s0x76bdbd188a4a98ab:0x160a089e92d5ce61!8m2!3d-16.5004126!4d-151.7414904 ",
+    startDate: "",
+    endDate: "",
+    description:
+      "Surrounded by sand-fringed motus (islets) and a turquoise lagoon protected by a coral reef",
+    imageUrl: "https://unsplash.com/photos/LFRUK7mslpk",
+  },
+];
