0a37e5cdc81a7fca96ab13482518be5bc778746b
andydangit <andy.dang.it@gmail.com>
2022-03-08 15:59:18 -0600

    Feat: Add the more data on page

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 function Card(props) {
   return (
-    <div>
+    <div>        
+      <img src={props.imageUrl} className="imgLogo" width="150px" alt = ""/>
+      <p>Location: {props.location}  </p>
+      <a href={props.googleMapsUrl}> View on Google map </a>
         <p>Title: {props.title} </p>
-        <p>Location: {props.location}  </p>
-        <img src={props.imageUrl} alt = ""/>
+        <span> {props.startDate} - {props.endDate} </span> 
+        <p> {props.description}</p>
+
+
 
     </div>
   )
