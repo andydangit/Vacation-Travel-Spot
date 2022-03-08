126a074aa26f7eb33ce024380ce68391ffbbcc66
andydangit <andy.dang.it@gmail.com>
2022-03-08 16:21:23 -0600

    Feat: Add Inline Style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 function Card(props) {
+
+  const cardStyle = {
+    padding: "25px",
+
+    
+  }
+
+  const cardImgStyle = {
+    width: "200px"
+  }
+
   return (
-    <div>        
-      <img src={props.imageUrl} className="imgLogo" width="150px" alt = ""/>
+    <div  style={cardStyle}>        
+      <img src={props.imageUrl} className="imgLogo" style={cardImgStyle} alt = ""/>
       <p>Location: {props.location}  </p>
       <a href={props.googleMapsUrl}> View on Google map </a>
         <p>Title: {props.title} </p>
