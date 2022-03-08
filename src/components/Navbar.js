35acc7a6d91953a985bd73c98dd070826f4631e9
andydangit <andy.dang.it@gmail.com>
2022-03-08 16:11:38 -0600

    Feat: Add navbar inline style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
+  const navbarStyle = {
+    backgroundColor: "#F55A5A",
+    // height: "30px",
+    margin: 0,
+    textAlign: "center",
+    padding: "20px",
+    color: "#fff",
+  };
+
   return (
-    <div>Vacation Spot</div>
-  )
+    <div>
+      <h1 className="container" style={navbarStyle}>
+        Vacation Spots
+      </h1>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
