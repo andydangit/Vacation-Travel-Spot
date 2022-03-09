cb6b909329a385b0e58af72cc1ca4f26427256bf
andydangit <andy.dang.it@gmail.com>
2022-03-09 16:18:28 -0600

    Refactor: update Start Date

--- src/Data.js
+++ src/Data.js
@@ -17,7 +17,7 @@ export default [
     location: "Vietnam",
     googleMapsUrl:
       "https://www.google.com/maps/place/H%E1%BA%A1+Long+Bay/@20.8432386,106.8701231,10z/data=!3m1!4b1!4m5!3m4!1s0x314a5796518cee87:0x55c6b0bcc85478db!8m2!3d20.9100512!4d107.1839024 ",
-    startDate: "",
+    startDate: "TBD",
     endDate: "",
     description:
       " Ha Long Bay  is known for its emerald waters and thousands of towering limestone islands topped by rainforests.",
@@ -30,7 +30,7 @@ export default [
     location: "Maldives",
     googleMapsUrl:
       "https://www.google.com/maps/place/Maldives/@3.1025792,68.7534806,6z/data=!3m1!4b1!4m5!3m4!1s0x24b599bfaafb7bbd:0x414509e181956289!8m2!3d3.202778!4d73.22068",
-    startDate: "",
+    startDate: "TBD",
     endDate: "",
     description:
       "Maldives, is an archipelagic country in the Indian subcontinent of Asia, situated in the Indian Ocean. It lies southwest of Sri Lanka and India, about 750 kilometres from the Asian continent's mainland",
@@ -43,7 +43,7 @@ export default [
     location: "Hawaii",
     googleMapsUrl:
       "https://www.google.com/maps/place/Hawaii/@20.4025299,-161.9895576,6z/data=!3m1!4b1!4m5!3m4!1s0x7bffdb064f79e005:0x4b7782d274cc8628!8m2!3d19.8967662!4d-155.5827818",
-    startDate: "",
+    startDate: "TBD",
     endDate: "",
     description:
       "Hawaii is a state in the Western United States, located in the Pacific Ocean about 2,000 miles from the U.S. mainland. ",
@@ -56,7 +56,7 @@ export default [
     location: "French Island",
     googleMapsUrl:
       "https://www.google.com/maps/place/Bora+Bora/@-16.5044211,-151.7735829,13z/data=!3m1!4b1!4m5!3m4!1s0x76bdbd188a4a98ab:0x160a089e92d5ce61!8m2!3d-16.5004126!4d-151.7414904 ",
-    startDate: "",
+    startDate: "TBD",
     endDate: "",
     description:
       "Surrounded by sand-fringed motus islets and a turquoise lagoon protected by a coral reef",
