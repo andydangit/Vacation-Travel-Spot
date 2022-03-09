bc47164bdb74bb3146e5212367f1c71a5c1abcc7
andydangit <andy.dang.it@gmail.com>
2022-03-09 15:31:49 -0600

    Feat: Add direction of img and text

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,26 +3,47 @@ import React from 'react'
 function Card(props) {
 
   const cardStyle = {
-    padding: "25px",
-
-    
+    padding: "25px",   
   }
 
   const cardImgStyle = {
-    width: "200px"
+    width: "400px",
+    height: "300px",
+    paddingLeft: "40px"
+  }
+
+  const column = {
+    display: "flex",
+    flexDirection: "column",
+    flexBasis: "40%",
+    flex: 1,
+    
+  }
+
+  const cardRow ={
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: "50%",
+    
   }
 
   return (
-    <div  style={cardStyle}>        
-      <img src={props.imageUrl} className="imgLogo" style={cardImgStyle} alt = ""/>
+    <div  style={cardStyle}>   
+    <div style={cardRow}>     
+      <div style={column}> 
+        <img src={props.imageUrl} className="imgLogo" style={cardImgStyle} alt = ""/>
+        </div> 
+
+        <div style ={column}> 
       <p>Location: {props.location}  </p>
       <a href={props.googleMapsUrl}> View on Google map </a>
         <p>Title: {props.title} </p>
         <span> {props.startDate} - {props.endDate} </span> 
         <p> {props.description}</p>
+    </div> 
 
-
-
+</div> 
     </div>
   )
 }
