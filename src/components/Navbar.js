fa7fb6bc95d093740e48e56191fb4b946190355c
andydangit <andy.dang.it@gmail.com>
2022-03-09 14:29:34 -0600

    Feat: Add globe icon to title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {GiGlobe} from 'react-icons/gi'
 
 function Navbar() {
   const navbarStyle = {
@@ -6,15 +7,22 @@ function Navbar() {
     // height: "30px",
     margin: 0,
     textAlign: "center",
-    padding: "20px",
+    padding: "15px",
     color: "#fff",
   };
+
+  const mainLogo = { 
+    height: "50px",
+    width: "45px",
+    paddingRight: "8px"
+  }
 
   return (
     <div>
       <h1 className="container" style={navbarStyle}>
-        Vacation Spots
+  <GiGlobe style={mainLogo} />  Vacation Spots
       </h1>
+      
     </div>
   );
 }
